3f9d7aac9366166cb07fbf42409ee2e83c1d93dc
Christian Ejnarsson <x_chrej@neolith1.(none)>
2009-06-10 17:47:57 +0200

    switched to jacobi subroutine

--- OpenMP-HTF.parallel2.f90
+++ OpenMP-HTF.parallel2.f90
@@ -4,7 +4,7 @@ program openmphtf
 	integer, parameter  								:: n = 1000
 	double precision,parameter			         :: tol=1.0E-3
 
-	integer												:: j,i
+	integer												:: j,i,k
 	!real 												:: T(0,n+1),Tnew(0,n+1), error
 	double precision, dimension(0:n+1, 0:n+1) :: Tnew, T
 
@@ -25,62 +25,77 @@ program openmphtf
 		error = 0
 	start2 = OMP_get_wtime()
 
-!		!$omp do
-!			do j=0,n+1
-!				do i=0,n+1
-!					if( i == n+1) then
-!						Tnew(i,j) = 2.0D0
-!						T(i,j) = 2.0D0
-!					else if(j==0 .OR. j == n + 1) then
-!						Tnew = 1.0D0
-!					else
-!						Tnew(i,j) = 0.0D0
-!						T(i,j) = 0.0D0
-!					end if
+!!		!$omp do
+!!			do j=0,n+1
+!!				do i=0,n+1
+!!					if( i == n+1) then
+!!						Tnew(i,j) = 2.0D0
+!!						T(i,j) = 2.0D0
+!!					else if(j==0 .OR. j == n + 1) then
+!!						Tnew = 1.0D0
+!!					else
+!!						Tnew(i,j) = 0.0D0
+!!						T(i,j) = 0.0D0
+!!					end if
+!!				end do
+!!			end do
+!!		!$omp end do
+!	!$omp parallel default	(shared)
+!		!$omp do private(j) reduction(MAX: error)
+!				do j=1,n
+!					do i=1,n
+!						Tnew(i,j) = ( T(i-1,j) + T( i,j+1) + T( i , j-1 ) + T(i+2 , j) ) / 4
+!						error = max(error, abs(Tnew(i,j) - T(i,j) ))
+!					end do
 !				end do
-!			end do
-!		!$omp end do
-	!$omp parallel default	(shared)
-		!$omp do private(j) reduction(MAX: error)
-				do j=1,n
-					do i=1,n
-						Tnew(i,j) = ( T(i-1,j) + T( i,j+1) + T( i , j-1 ) + T(i+2 , j) ) / 4
-						error = max(error, abs(Tnew(i,j) - T(i,j) ))
-					end do
-				end do
-			!$omp end do
+!			!$omp end do
 
-			!$omp do
-				do j=1,n
-					do i=1,n
-						T(i,j)=Tnew(i,j)
-					end do
-				end do
-			!$omp end do
-	!$omp end parallel
-write(*,*) "KUK"
-		do while (error > tol)
-		error = 0.0D0
-	!$omp parallel default	(shared)
-		!$omp do private(j) reduction(MAX: error)
-				do j=1,n
-					do i=1,n
-						Tnew(i,j) = ( T(i-1,j) + T( i,j+1) + T( i , j-1 ) + T(i+2 , j) ) / 4
-						error = max(error, abs(Tnew(i,j) - T(i,j) ))
-					end do
-				end do
-			!$omp end do
+!			!$omp do
+!				do j=1,n
+!					do i=1,n
+!						T(i,j)=Tnew(i,j)
+!					end do
+!				end do
+!			!$omp end do
+!	!$omp end parallel
+!write(*,*) "KUK"
+!		do while (error > tol)
+!		error = 0.0D0
+!	!$omp parallel default	(shared)
+!		!$omp do private(j) reduction(MAX: error)
+!				do j=1,n
+!					do i=1,n
+!						Tnew(i,j) = ( T(i-1,j) + T( i,j+1) + T( i , j-1 ) + T(i+2 , j) ) / 4
+!						error = max(error, abs(Tnew(i,j) - T(i,j) ))
+!					end do
+!				end do
+!			!$omp end do
 
-			!$omp do
-				do j=1,n
-					do i=1,n
-						T(i,j)=Tnew(i,j)
-					end do
-				end do
-			!$omp end do
-			!write(*,*) "error: ", error
-	!$omp end parallel
-		end do
+!			!$omp do
+!				do j=1,n
+!					do i=1,n
+!						T(i,j)=Tnew(i,j)
+!					end do
+!				end do
+!			!$omp end do
+!			!write(*,*) "error: ", error
+!	!$omp end parallel
+!		end do
+
+error = 0.0D0
+k=1
+call jacobi(Tnew,T,n,error)
+write(*,*) k, ' error: ', error
+do while (error > tol)
+	error = 0.0D0
+	k=k+1
+	call jacobi(T, Tnew, n, error)
+	error = 0
+	k=k+1
+	call jacobi(Tnew, T, n, error)
+write(*,*) k,' error: ',	 error
+
+end do
 
 	end1 = OMP_get_wtime()
 
@@ -95,19 +110,19 @@ write(*,*) "KUK"
 end program openmphtf
 
 subroutine jacobi(T, Tnew, n, error)
-	real :: error
+	real(8) :: error
 	integer :: n
 	double precision, dimension (0:n+1,0:n+1) :: T, Tnew
-	!$omp parallel default	(shared)
-		!$omp do private(j) reduction(MAX: error)
+	!!$omp parallel default	(shared)
+	!	!$omp do private(j) reduction(MAX: error)
 				do j=1,n
 					do i=1,n
 						Tnew(i,j) = ( T(i-1,j) + T( i,j+1) + T( i , j-1 ) + T(i+2 , j) ) / 4
 						error = max(error, abs(Tnew(i,j) - T(i,j) ))
 					end do
 				end do
-			!$omp end do
-	!$omp end parallel
+	!		!$omp end do
+	!!$omp end parallel
 end subroutine jacobi
 
 

d631ab9d4ee20bec3867aeaf344788d4f794bfae
Christian Ejnarsson <x_chrej@neolith1.(none)>
2009-06-13 11:58:03 +0200

    ...parallel3.f90 added, based on ...parallel.f90. Working, subroutine jacbi added but not yet called.

---
+++ OpenMP-HTF.parallel3.f90
@@ -0,0 +1,144 @@
+program laplsolv
+	!-----------------------------------------------------------------------
+	! Serial program for solving the heat conduction problem
+	! on a square using the Jacobi method.
+	! Written by Fredrik Berntsson (frber@math.liu.se) March 2003
+	! Modified by Berkant Savas (besav@math.liu.se) April 2006
+	!-----------------------------------------------------------------------
+	use omp_lib
+	integer, parameter                  :: nmax=1000, maxiter=500
+	integer										:: n, threads
+	double precision,parameter          :: tol=1.0E-3
+	!double precision          :: tol=1.0E-3
+	double precision,dimension(0:nmax+1,0:nmax+1) :: T1,T2
+	double precision                    :: error,x
+	integer                             :: i,j,k, solution
+	character(len=20)                   :: str
+
+	!Timing variables
+	real(8)										:: start_t, end_t
+
+	!CLI argument buffer
+	character*100								::	buffer
+	character(len=30)							:: filename
+
+	call getarg(1, buffer)
+	read(buffer,*) threads
+	call getarg(2, buffer)
+	read(buffer,*) filename
+
+	n = nmax
+	if(threads > 8) then
+		threads = 8
+	end if
+	call omp_set_num_threads(threads)
+
+	! Set boundary conditions and initial values for the unknowns
+	T1=0.0D0
+	T1(0:n+1 , 0)     = 1.0D0
+	T1(0:n+1 , n+1)   = 1.0D0
+	T1(n+1   , 0:n+1) = 2.0D0
+	T2=0.0D01
+	T2(0:n+1 , 0)     = 1.0D0
+	T2(0:n+1 , n+1)   = 1.0D0
+	T2(n+1   , 0:n+1) = 2.0D0
+	solution = 0
+
+	start_t = OMP_get_wtime()
+
+	! Solve the linear system of equations using the Jacobi method
+	do k=1,maxiter
+		solution = 0;
+		error=0.0D0
+!$omp parallel default(shared)
+
+!$omp   do  schedule(guided, 1) reduction(MAX:error)
+		do j=1,n
+			T2(1:n,j)= ( T1(0:n-1,j) + T1(2:n+1,j) + T1(1:n,j+1) + T1(1:n,j-1) ) / 4.0D0
+			error=max(error,maxval(abs(T2(1:n,j)-T1(1:n,j))))
+		end do
+!$omp end do
+
+!$omp master
+		write(*,*) 2*k-1, ' error: ', error
+		if (error<tol) then
+			solution = solution + 2
+		end if
+		error=0.0D0
+!$omp end master
+
+!$omp  do  schedule(guided, 1) reduction(MAX:error)
+		do j=1,n
+			T1(1:n,j)= ( T2(0:n-1,j) + T2(2:n+1,j) + T2(1:n,j+1) + T2(1:n,j-1) ) / 4.0D0
+			error=max(error,maxval(abs(T2(1:n,j)-T1(1:n,j))))
+		end do
+!$omp end  do
+
+!$omp master
+		write(*,*) 2*k, ' error: ', error
+		if (error<tol) then
+			solution = solution + 1
+		end if
+!$omp end master
+
+!$omp end parallel
+
+		if(solution > 0) then
+			exit
+		end if
+	end do
+
+	end_t = OMP_get_wtime()
+	start_t = end_t - start_t
+	k = k*2 - (min(2,solution) - 1)
+
+	write(unit=*,fmt='(a,f7.3,a,i5.3)') 'Time:',start_t,'Number of Iterations:',k
+	if (solution < 2) then
+		write(unit=*,fmt=*) 'Temperature of element Tx(1,1)  =',T1(1,1)
+	else
+		write(unit=*,fmt=*) 'Temperature of element Tx(1,1)  =',T2(1,1)
+	end if
+
+	open(unit=7,action='write',file=filename,status='unknown')
+	write(unit=str,fmt='(a,i6,a)') '(',N,'F10.6)'
+	write(unit=7, fmt='(A,I2,A,I6,A,I6,A,F7.3)') 'threads=', threads, ' n =', n, ' iterations =', k, ' time-to-solve =', start_t
+	if(solution == 1) then
+		do i=0,n+1
+			write (unit=7,fmt=str) T1(i,0:n+1)
+		end do
+	else
+		do i=0,n+1
+			write (unit=7,fmt=str) T2(i,0:n+1)
+		end do
+	end if
+	close(unit=7)
+
+end program laplsolv
+
+
+subroutine jacobi (Told, Tnew, n, solution, tol)
+	double precision,dimension(0:nmax+1,0:nmax+1) 	:: Tnew,Told
+	double precision											:: tol, error
+	integer														:: n, solution
+
+
+!$omp master
+	error = 0.0D0
+!$omp end master
+
+!$omp   do  schedule(guided, 1) reduction(MAX:error)
+	do j=1,n
+		Tnew(1:n,j)= ( Told(0:n-1,j) + Told(2:n+1,j) + Told(1:n,j+1) + Told(1:n,j-1) ) / 4.0D0
+		error=max(error,maxval(abs(Tnew(1:n,j)-Told(1:n,j))))
+	end do
+!$omp end do
+
+!$omp master
+	write(*,*) 2*k-1, ' error: ', error
+	if (error<tol) then
+		solution = solution + 2
+	end if
+!$omp end master
+
+end subroutine
+
---
+++ build.parallel3.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ifort -openmp -g -p -traceback OpenMP-HTF.parallel3.f90 -o OpenMP-HTF.parallel3
